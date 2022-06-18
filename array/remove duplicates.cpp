33943660145ada7d4e9eb4e481afcdc36f1c924f
SachinKapkoti <37322308+sachinstonecold@users.noreply.github.com>
2022-06-18 18:31:27 +0530

    Add files via upload

---
+++ array/remove duplicates.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+class Solution {
+   public:
+   int removeDuplicates(vector<int>& nums) {
+      //Write your code here
+      int n=nums.size();
+      int l=0;
+      for(int r=1;r<n;r++)
+      {
+          if(nums[r]!=nums[l])//  compare 2 value with 1st value 
+          l++;
+          nums[l]=nums[r]     ;   //we put new found element inplace
+          
+      }
+      return l+1;
+   
+   }
+};
+int main(){
+   Solution ob;
+   vector<int> v = {0,0,0,1,1,1,1,2,3,3};
+   cout << ob.removeDuplicates(v);
+}
