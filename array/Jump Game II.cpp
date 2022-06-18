7d00f77cef7739299397b1605f36c6d1c1d1ece1
SachinKapkoti <37322308+sachinstonecold@users.noreply.github.com>
2022-06-18 15:11:55 +0530

    Add files via upload

---
+++ array/Jump Game II.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int max_reach=0, curr_reach=0, jumps=0;
+        
+        for(int i=0;i<nums.size()-1;i++){
+            max_reach = max(max_reach, nums[i]+i);
+            if(i==curr_reach) curr_reach = max_reach, jumps++;   
+        }
+        return jumps;
+    }
+};
