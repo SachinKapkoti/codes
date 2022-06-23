2aed850cda2cfc60db02c6684e57c6d434168dd5
SachinKapkoti <37322308+sachinstonecold@users.noreply.github.com>
2022-06-23 23:51:32 +0530

    Update Jump Game II.cpp

--- array/Jump Game II.cpp
+++ array/Jump Game II.cpp
@@ -5,7 +5,9 @@ public:
         
         for(int i=0;i<nums.size()-1;i++){
             max_reach = max(max_reach, nums[i]+i);
-            if(i==curr_reach) curr_reach = max_reach, jumps++;   
+            { if(i==curr_reach)     //found ele where we can jump to last
+                curr_reach = max_reach, jumps++;   
+            }
         }
         return jumps;
     }
