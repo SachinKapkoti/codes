035c6267146d2fc55acf2a8aca82673b38ba4540
SachinKapkoti <37322308+sachinstonecold@users.noreply.github.com>
2022-07-19 12:48:54 +0530

    Add files via upload

---
+++ tree/inorder recursive.cpp
@@ -0,0 +1,23 @@
+class Solution {
+  public:
+   
+    // Function to return a list containing the inorder traversal of the tree.
+   
+   void inorder(Node* root,vector<int>&ans)
+   {
+       if(!root==NULL)
+       {
+         inorder(root->left,ans);
+        ans.push_back(root->data);
+        inorder(root->right,ans);
+       }
+   }
+   
+    vector<int> inOrder(Node* root) {
+        // Your code here
+        vector<int>ans;
+        inorder(root,ans);
+    
+        return ans;
+    }
+};
