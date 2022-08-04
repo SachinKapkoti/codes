9ce727aee19faf7a3b6a7baa4c8339690d5026da
SachinKapkoti <37322308+sachinstonecold@users.noreply.github.com>
2022-08-04 11:15:01 +0530

    Add files via upload

---
+++ Linked list/intersection point in LL.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *getIntersectionNode(ListNode *headA, ListNode *headB) {
+        
+        ListNode* l1=headA;
+         ListNode* l2=headB;
+        
+        
+        if(!l1 || !l2)//if any one not there no intersection
+            return NULL;
+        
+        while(l1!=l2)
+        { 
+            if(!l1)
+                l1=headB;
+            else l1=l1->next;
+            
+            if(!l2)
+                 l2=headA;
+            else l2=l2->next;
+            
+        }
+        //if equal then intesection point
+        return l1;
+        
+        }
+};
