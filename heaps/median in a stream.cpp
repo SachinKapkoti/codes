d90541de02c25d2f0065059969ffa718bab92b31
SachinKapkoti <37322308+sachinstonecold@users.noreply.github.com>
2022-08-09 12:06:52 +0530

    Add files via upload

---
+++ heaps/median in a stream.cpp
@@ -0,0 +1,44 @@
+class MedianFinder {
+public:
+    
+    priority_queue<int>s;
+    priority_queue<int,vector<int>,greater<int>>g;
+    
+    MedianFinder() {}
+    
+    void addNum(int num) {
+        if(s.size()==0 || s.top()>num)
+        { s.push(num);
+        }
+        else
+        {g.push(num);}
+        
+        if(s.size()>g.size()+1)
+        {
+            g.push(s.top());
+            s.pop();
+        }
+        if(g.size()>s.size())
+        {
+            s.push(g.top());
+            g.pop();
+        }
+        
+    }
+    
+    double findMedian() {
+        if(g.size()==s.size())
+            
+            return (g.top()+s.top())/2.000;
+        
+        else
+            return s.top();
+    }
+};
+
+/**
+ * Your MedianFinder object will be instantiated and called as such:
+ * MedianFinder* obj = new MedianFinder();
+ * obj->addNum(num);
+ * double param_2 = obj->findMedian();
+ */
